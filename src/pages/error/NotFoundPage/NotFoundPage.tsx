7e4584619096ab8fb0adb73104e550029f79ea9a
Jerry Endaya <vagabondendaya@gmail.com>
2026-03-07 18:23:33 +0800

    Adjusted the height and width of the icons.

--- src/pages/error/NotFoundPage/NotFoundPage.tsx
+++ src/pages/error/NotFoundPage/NotFoundPage.tsx
@@ -67,7 +67,7 @@ export const NotFoundPage = () => {
             leftIcon={
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-4 w-4"
+                className="h-6 w-6"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
